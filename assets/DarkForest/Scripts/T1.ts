b9f61f8e992b9da04d01c9173936eb9365d0c26b
topsaler <borostudio2018@gmail.com>
2022-12-06 16:31:34 +0700

    - Scene Closed to begin hot update test

--- assets/DarkForest/Scripts/T1.ts
+++ assets/DarkForest/Scripts/T1.ts
@@ -16,27 +16,7 @@ export class T1 extends Component {
     @property({type:sp.Skeleton})
     monster: sp.Skeleton = null;
 
-    loadSpineTest(){
-        SpineManager.instance().loadSpine(
-            'DarkForest','Resources/Animation/spine/spineboy/spineboy-pro',
-            (error:number, spineKey:string, spineData?:sp.SkeletonData)=>{
-                log('loadSpineTest', error, spineKey);
-                if(error == 0){
-                    log('create spine 1');
-                    let spine = new sp.Skeleton();
-                    log('create spine 2');
-                    spine.skeletonData = spineData;
-                    log('create spine 3');
-                    // let node = instantiate(spine);
-                    // this.node.addChild(node);
-                    log('create spine 4');
-                }
-            }
-        )
-    }
-
     start() {
-        this.loadSpineTest();
         for(var i = 0; i < this.menu.length; i++){
             const index = i;
             this.menu[index]
